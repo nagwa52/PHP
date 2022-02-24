c9a5c8d780661b661ebb5b464c2dd4c2b4404e88
nagwa52 <nagwatalaat52@gmail.com>
2022-02-24 06:32:30 +0200

    files done and form create and also globa;s

---
+++ Elzero/globals.php
@@ -0,0 +1,24 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+//-----------------------------------------------------------------------------------
+// Global variables
+//-----------------------------------------------------------------------------------
+   /* $name="nagwa";
+function displayName(){
+    $name= "Maher";
+    echo "The local variable is ". $name . "<br>";
+    echo "The global variable is ". $GLOBALS['name'] . "<br>";
+}
+displayName()
+*/
+
+    ?>
+</body>
+</html>
