1f305b0b515cb4605584eb04a5f20c1fe0d57c39
nagwa52 <nagwatalaat52@gmail.com>
2022-02-25 05:33:40 +0200

    add coockies with bg color

--- Elzero/cookies.php
+++ Elzero/cookies.php
@@ -3,11 +3,38 @@
 // setcookie(name,value,expired,path,domain,secure,httponly)
 //-----------------------------------------------------------------------------------
   
-setcookie("website","test", time() + 3600,"/");
-if(count($_COOKIE)>0){
-    echo "yes cookie is enabled";
+// setcookie("website","test", time() + 3600,"/");
+// if(count($_COOKIE)>0){
+//     echo "yes cookie is enabled";
+// }
+// else{
+//     echo "yes cookie is not enabled";  
+// }
+$mainColor ='#FFF';
+if($_SERVER['REQUEST_METHOD']=='POST'){
+     $mainColor= $_POST['colorname'];
+    setcookie('Background',$mainColor, time() + 3600,"/");
+}
+
+if(isset($_COOKIE['Background'])){
+    $body =  $_COOKIE['Background'];
 }
 else{
-    echo "yes cookie is not enabled";  
+    $body = $mainColor;
 }
+
 ?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body style="background-color :<?php echo $body ?>">
+<form action= "<?php echo $_SERVER['PHP_SELF'];?>" method='POST'> 
+       <input type="color" name="colorname" >
+       <input type="submit" value="choose" >
+</form>  
+</body>
+</html>
