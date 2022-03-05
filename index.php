8a0c250a44808875c7bf1be9a3b57152619f3a33
nagwa52 <nagwatalaat52@gmail.com>
2022-03-05 02:21:32 +0200

    add database and initial validation to email and password

--- index.php
+++ index.php
@@ -11,21 +11,30 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
     <body>
         <?php
         session_start();
-           require_once('vendor/autoload.php');
-          
-           use Illuminate\Database\Capsule\Manager as capsule;
-           $capsule = new capsule();
-//         $information=  $capsule->addConnection([
-//'driver'   => _driver_,
-//'host'     => _host_,
-//'database' => _database_,
-//'username' => _username_,
-//'password' => _password_,
-//]);
-           $capsule->setAsGlobal(); 
-           $capsule->bootEloquent();
-       require_once './View/Login.php';
-       
+        require_once('vendor/autoload.php');
+
+        use Illuminate\Database\Capsule\Manager as capsule;
+        $capsule = new capsule();
+        $capsule->addConnection([
+//            'driver' => __DRIVER__,
+//            'host' => __HOST__,
+//            'database' => __DATABASE__,
+//            'username' => __USERNAME__,
+//            'password' => __PASSWORD__
+             'driver' => "mysql",
+           'host' => "127.0.0.1",
+           'database' => "student",
+           'username' => "root",
+            'password' => "",
+            
+        ]);
+        $capsule->setAsGlobal();
+        $capsule->bootEloquent();
+        //(isset($_POST["index"])) ? echo 'welcome' : echo 'not registered';
+         
+           $record =  capsule::table('data')->get();
+          require_once './View/memberArea.php';
+        //  require_once './View/Login.php';
         ?>
     </body>
 </html>
